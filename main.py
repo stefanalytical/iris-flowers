cbc9d865a60b1afe762bf7622e42554b1beb42a0
Stefan Vuleta <vuleta99@gmail.com>
2022-08-05 00:30:33 -0400

    Removed KNeighborsClassifier

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
@@ -54,7 +53,6 @@ X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=
 models = []
 models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
 models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC(gamma='auto')))
