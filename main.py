461cb961834aa3886e231d3388ca4fcf5c1671b3
Stefan Vuleta <vuleta99@gmail.com>
2022-08-05 01:21:02 -0400

    Added comments for clarity

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Load libraries
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
@@ -14,70 +13,72 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-# Load dataset
+# Load CSV file as a Pandas DataFrame and set column names.
 path = '/Users/stefanvuleta/Projects/iris_machine_learning/iris.csv'
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(path, names = names)
 
-# shape
+# Find number of DataFrame rows and columns.
 print(dataset.shape)
 
-# head
+# Access first 20 rows in DataFrame.
 print(dataset.head(20))
 
-# descriptions
+# Calculate some statistical data like percentile, mean and std of the numerical values of the DataFrame.
 print(dataset.describe())
 
-# class distribution
+# Get int representing the number of elements in class column by group.
 print(dataset.groupby('class').size())
 
-# box and whisker plots
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# Create box and whisker plots.
+dataset.plot(kind = 'box', subplots = True, layout = (2,2), sharex = False, sharey = False)
 pyplot.show()
 
-# histograms
+# Create four histograms.
 dataset.hist()
 pyplot.show()
 
-# scatter plot matrix
+# Create a scatter plot matrix to visualize bivariate relationships.
 scatter_matrix(dataset)
 pyplot.show()
 
-# Split-out validation dataset
+# Split-out validation dataset. Dataset split into 2 parts. 80% will be used to train, evaluate and select among models.
+# 20% will be held back as a validation dataset.
 array = dataset.values
 X = array[:,0:4]
 y = array[:,4]
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size = 0.20, random_state=1)
 
-# Spot Check Algorithms
+# Spot check algorithms.
 models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
+models.append(('LR', LogisticRegression(solver = 'liblinear', multi_class = 'ovr'))) # Logistic Regression. Simple linear algorithm.
+models.append(('LDA', LinearDiscriminantAnalysis())) # Linear Discriminant Analysis. Simple linear algorithm.
+models.append(('CART', DecisionTreeClassifier())) # Classification and Regression Trees. Nonlinear algorithm.
+models.append(('NB', GaussianNB())) # Gaussian Naive Bayes. Nonlinear algorithm.
+models.append(('SVM', SVC(gamma = 'auto'))) # Support Vector Machines. Nonlinear algorithm.
 
-# evaluate each model in turn
+# Evaluate each model. Results may vary given the stochastic nature of the algorithm or evaluation procedure or differences in numerical precision.
 results = []
 names = []
 for name, model in models:
-	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	kfold = StratifiedKFold(n_splits = 10, random_state = 1, shuffle = True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv = kfold, scoring = 'accuracy')
 	results.append(cv_results)
 	names.append(name)
 	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 
-# Compare Algorithms
+# Compare algorithms by creating a box and whisker plot for each distribution.
 pyplot.boxplot(results, labels = names)
 pyplot.title('Algorithm Comparison')
 pyplot.show()
 
-# Make predictions on validation dataset
-model = SVC(gamma='auto')
+# Fit the model on the entire training dataset and make predictions on the validation dataset.
+model = SVC(gamma = 'auto')
 model.fit(X_train, Y_train)
 predictions = model.predict(X_validation)
 
-# Evaluate predictions
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
+# Evaluate predictions by comparing them to the expected results in the validation set. Then calculate classification accuracy,
+# as well as a confusion matrix and a classification report.
+print(accuracy_score(Y_validation, predictions)) # Accuracy is .966 or about 96% on the dataset.
+print(confusion_matrix(Y_validation, predictions)) # Provides an indication of the errors made.
+print(classification_report(Y_validation, predictions)) # Provides breakdown of each class by precision, recall, f1-score, and support.
